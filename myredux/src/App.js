edbc1bb37e9197141cab1752909f7e1f2d1da71e
Ale <alearanibar@gmail.com>
2019-11-07 22:34:40 -0800

    redux

---
+++ myredux/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MyDataList from './components/MyDataList'
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Famous Breweries</h1>
+     <MyDataList />
+    </div>
+  );
+}
+
+export default App;
